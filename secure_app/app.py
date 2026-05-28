c3345df4490f2513d8ce4720915f42414f1677a3
Luan Camargo <126421528+LuanAlvesCamargo@users.noreply.github.com>
2026-05-28 20:03:39 -0300

    Pagina de documentação

--- secure_app/app.py
+++ secure_app/app.py
@@ -238,4 +238,4 @@ def logout():
 
 # RUN APPLICATION
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
--- vulnerable_app/app.py
+++ vulnerable_app/app.py
@@ -276,4 +276,4 @@ def logout():
 
 # RUN APPLICATION
 if __name__ == "__main__":
-    app.run(debug=True)
+   app.run(debug=True, port=5000)
